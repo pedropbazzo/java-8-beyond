afa9f3568468e5112ae6964aa0cd9466721324b9
pedropbazzo <pedropbazzo@outlook.com>
2021-11-02 14:32:56 -0300

    first commit

---
+++ src/main/java/com/javaoito/java/polymorphism/Cat.java
@@ -0,0 +1,13 @@
+package com.javaoito.java.polymorphism;
+
+                  //IS A/AN
+public class Cat extends Animal {
+
+
+    @Override
+    public void makeSound(){
+        System.out.println("I am cat. meow....");
+    }
+
+
+}
